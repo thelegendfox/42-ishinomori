e37b5f43a836496a9b00a3908063b842cb2caec2
thelegendfox <172534204+thelegendfox@users.noreply.github.com>
2025-09-03 11:44:45 -0500

    driving in my car, right after a beer, hey that bu

--- index.ts
+++ index.ts
@@ -16,8 +16,8 @@ declare module "discord.js" {
 /* prettier-ignore */
 import { Client, Events, GatewayIntentBits, Collection, MessageFlags } from "discord.js";
 import getDifference from "./util/getDifference.ts";
-// import path from "node:path";
-// import fs from "node:fs";
+import path from "node:path";
+import fs from "node:fs";
 // import { fileURLToPath } from "node:url";
 import update from "./commands/update/update.ts";
 import sql from "./util/database.ts";
@@ -42,91 +42,34 @@ client.login(process.env.DISCORD_TOKEN);
 
 //
 
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith(".ts"));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ("data" in command && "execute" in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
+		}
+	}
+}
+
 client.on(Events.Error, (err: Error) => {
 	console.error(err);
 	// process.exit();
 });
 
 async function main(): Promise<void> {
-	client.on(Events.GuildBanAdd, (ban) => {
-		//
-	});
-	client.on(Events.GuildBanRemove, (ban) => {
-		//
-	});
-	client.on(Events.GuildMemberAdd, (member) => {
-		//
-	});
-	client.on(Events.GuildMemberRemove, (member) => {
-		//
-	});
-	client.on(Events.UserUpdate, (oldUser, newUser) => {
-		/*
-
-        Tracks the following:
-            -Updated avatars
-            -Updated usernames (NOT global display names)
-
-        */
-
-		// if(oldUser.bot || newUser.bot) return;
-		// Turn back on in prod
-
-		if (oldUser.avatar !== newUser.avatar) {
-			console.log(
-				`User avatar hash changed from "${oldUser.avatar}" to "${newUser.avatar}" (user ID: "${newUser.id}")`
-			);
-		}
-		if (oldUser.username !== newUser.username) {
-			console.log(
-				`User username changed from "${oldUser.username}" to "${newUser.username}" (user ID: "${newUser.id}")`
-			);
-		}
-	});
-	client.on(Events.GuildMemberUpdate, (oldMember, newMember) => {
-		/*
-
-        Tracks the following:
-            -Updated roles
-            -Updated display names
-
-        */
-
-		// if(oldUser.bot || newUser.bot) return;
-		// Turn back on in prod
-
-		let oldRoles: Map<String, String> = new Map();
-		let newRoles: Map<String, String> = new Map();
-
-		// Do this to easily compare old and new roles (@see: getDifferences)
-		oldMember.roles.cache.forEach((role) =>
-			oldRoles.set(role.id, role.name)
-		);
-		newMember.roles.cache.forEach((role) =>
-			newRoles.set(role.id, role.name)
-		);
-
-		const differentRoles = getDifference(oldRoles, newRoles) ?? [];
-
-		// Log all differences in roles.
-		for (const role of differentRoles) {
-			if (!differentRoles) break;
-
-			const id = role[0];
-			const info = {
-				name: role[1][0],
-				action: role[1][1],
-			};
-			console.log(`Role ${info.action}: ${info.name} (${id})`);
-		}
-
-		// Log differences in display names (only if there are differences)
-		if (oldMember.displayName !== newMember.displayName)
-			console.log(
-				`Member display name changed from "${oldMember.displayName}" to "${newMember.displayName}" (Member ID: "${newMember.id}")`
-			);
-	});
-
 	// Most of this is from discord.js
 	client.on(Events.InteractionCreate, async (interaction) => {
 		if (!interaction.isChatInputCommand()) return;
@@ -139,25 +82,31 @@ async function main(): Promise<void> {
 			console.error(
 				`No command matching ${interaction.commandName} was found.`
 			);
+			interaction.reply({
+				content:
+					"We're sorry! There was an error and the command could not be called.",
+				flags: MessageFlags.Ephemeral,
+			});
 			return;
 		}
 
-		try {
-			await command.execute(interaction);
-		} catch (error) {
+		await command.execute(interaction).catch(async (error: Error) => {
 			console.error(error);
+
 			if (interaction.replied || interaction.deferred) {
 				await interaction.followUp({
-					content: "There was an error while executing this command!",
+					content:
+						"We're sorry! There was an error while executing this command!",
 					flags: MessageFlags.Ephemeral,
 				});
 			} else {
 				await interaction.reply({
-					content: "There was an error while executing this command!",
+					content:
+						"We're sorry! There was an error while executing this command!",
 					flags: MessageFlags.Ephemeral,
 				});
 			}
-		}
+		});
 	});
 }
 
--- util/database.ts
+++ util/database.ts
@@ -1,10 +1,11 @@
-import postgres from "postgres";
+import database from "better-sqlite3";
 import dotenv from "dotenv";
 dotenv.config();
 
 /**
  * The standard SQL operator.
  */
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+const sql: unknown = undefined;
 
 export default sql;
