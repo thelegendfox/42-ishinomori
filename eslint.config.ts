4e94a265394e82b725437623c81e3349bf9c8a55
thelegendfox <172534204+thelegendfox@users.noreply.github.com>
2025-09-15 13:45:06 -0500

    Okay I guess it ignored my commit. Rules 'n stuff

--- eslint.config.ts
+++ eslint.config.ts
@@ -35,7 +35,7 @@ export default defineConfig([
 		rules: {
 			"@typescript-eslint/no-unused-vars": "off",
 			"@typescript-eslint/no-explicit-any": "off",
-			// "prefer-const": "off",
+			"prefer-const": "off",
 		},
 	},
 ]);
